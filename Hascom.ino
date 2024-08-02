97212b1376649c855dbfbfa54bcf07b0db7b0a33
Dennis Schwartze <dennis.schwartze@gmail.com>
2024-08-02 11:18:35 +0200

    Update Hascom.ino

--- Hascom.ino
+++ Hascom.ino
@@ -1,10 +1,11 @@
 
+#include <EEPROM.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #define ONE_WIRE_BUS 10
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-
+boolean Sommerzeit;
 
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
@@ -28,13 +29,14 @@ SimpleDHT11 dht11(2);
  */
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(3,4,5,6,7,8);
-byte jahr,monat,tag,stunde,mimute,sekunde,zeitzone=2;//Zeitzone für sommer/winterzeit usw
-byte uzeit=zeitzone+1;
+byte jahr,monat,tag,stunde,mimute,sekunde,zeitzone;//Zeitzone für sommer/winterzeit usw
 byte Menu;
 byte L,M,R;
 double uMin,uMax,uMinabs=1023,uMaxabs,uAvr,uNow,uRip,uRipabs;
 void setup() {
-
+  
+  EEPROM.get(0, Sommerzeit);
+  zeitzone=1+Sommerzeit;
   digitalWrite(A1,HIGH);
   digitalWrite(A2,HIGH);
   digitalWrite(A3,HIGH);
@@ -45,24 +47,25 @@ void setup() {
 
 }
 void lcdclean(){ // LcdBildschirm Löschen
-  lcd.setCursor(0,0);
+ /* lcd.setCursor(0,0);
   lcd.print("        ");
   lcd.setCursor(0,1);
   lcd.print("        ");
-  }
+  */}
 void getTemp(){ // Hole die Tempereatur und die Luftfeuchtigkeit und speichere sie in Var: temperature und humidity
    err = dht11.read(&temperature, &humidity, NULL);
   }
 void showClima(){// Zeige Temperatur und Luftfeuchte im Fahrzeug an
+  if(((millis()/1000)%10)==0)sensors.requestTemperatures();// Hole Temperaturen der Tempsensoren
   lcdclean();
   getTemp();
   lcd.setCursor(0,0);
  
   lcd.print((int)temperature);
-  lcd.print("C bei");
+  lcd.print("C bei ");
   lcd.setCursor(0,1);
   lcd.print((int)humidity);
-  lcd.print("% rel");
+  lcd.print("% rel ");
   }
 void getClock(){
     while (ss.available() > 0)
@@ -85,7 +88,7 @@ void getClock(){
     stunde=stunde+zeitzone;
     stunde=stunde%24;
    
-    if (stunde<uzeit)//überlaufzeit
+    if (stunde<zeitzone)//überlaufzeit
     {
       tag++;     
       if (tag==32){       tag=1;  monat++;} //wenn der monat 32tage hat
@@ -124,13 +127,15 @@ void showClock(){
         
     lcd.print(jahr);
  
-    lcd.setCursor(1, 1); 
+    lcd.setCursor(0, 1); 
+    lcd.print(" ");
 //  lcd.setCursor(0,1); //falls sekunden angezeigt werden sollen
     if (stunde<10) lcd.print("0");
     lcd.print(stunde); lcd.print(":");
     
     if (mimute<10) lcd.print("0");
     lcd.print(mimute);
+    lcd.print("  ");
     
 /*
     lcd.print(":");
@@ -144,19 +149,20 @@ void getVolt(){
   uAvr=0;
   uMin=1023;
   uMax=0;
-  for(x=0;x<200;x++){
+  for(x=0;x<50;x++){
       uNow=5.0*analogRead(A0)/1023.0;
       uAvr+=uNow;
       if (uNow>uMax)uMax=uNow;
       if (uNow<uMin)uMin=uNow;
     }
-   uAvr/=200;
+   uAvr/=50;
    uRip=uMax-uMin;
    if (uRipabs<uRip)uRipabs=uRip;
    if (uMaxabs<uMax)uMaxabs=uMax;
    if (uMinabs>uMin)uMinabs=uMin;
   }
 void showVolt(){
+  getVolt();// Messe Boardspannung
   lcdclean();
   lcd.setCursor(0,0);
   lcd.print("Um=");lcd.print(uAvr);lcd.print("V");
@@ -164,6 +170,7 @@ void showVolt(){
   lcd.print("Ud=");lcd.print(uRip);lcd.print("V");
   }
 void showTemp(){
+  
   lcdclean();
   lcd.setCursor(0,0);
   lcd.print("T1=");lcd.print(sensors.getTempCByIndex(0));
@@ -172,34 +179,43 @@ void showTemp(){
 }
 byte getButton(){
   byte doit=0;
-  if (digitalRead(A1))L=0; else {L=1;doit=1;}
-  if (digitalRead(A2))M=0; else {M=1;doit=2;}
-  if (digitalRead(A3))R=0; else {R=1;doit=3;}
-  
-  if ((L+M+R)==1) return doit;
-  if (((L+M+R)==0)||((L+M+R)>1)) {doit=0;return doit;}
+  unsigned long timer=millis();
+  if (!digitalRead(A2)){
+    lcd.setCursor(0,1);
+    lcd.print("So/Wi? ");
+    }
+  while(!digitalRead(A2)){
     
-    
-  
+    if ((Menu==0)&&((millis()-timer)>5000)){
+      if (Sommerzeit)Sommerzeit=0; else Sommerzeit=1;
+      zeitzone=1+Sommerzeit;
+      lcd.print("!");
+      EEPROM.put(0, Sommerzeit);
+      delay(1000);
+      break;}
+    }
+  while (!digitalRead(A1)or!digitalRead(A3)){
+      if (digitalRead(A1))L=0; else {L=1;doit=64;}
+      if (digitalRead(A3))M=0; else {M=1;doit=192;}
+      
+    }
+    return doit; 
   }
 void goRound(){
   
   //Rotation der Menüs
   }
-void loop() { 
-int doit;
-  if ((millis()%200)==0){
-  sensors.requestTemperatures();// Hole Temperaturen der Tempsensoren
-  getVolt();// Messe Boardspannung
-  if (Menu==0)showClock();
-  if (Menu==64)showClima();
-  if (Menu==128)showTemp();
-  if (Menu==192)showVolt();}
-  if (doit==0) doit=getButton();
   
-  if ((millis()%100)==0){
-    if (doit==1) Menu+=64;
-    if (doit==3) Menu-=64;
-    doit=0;
-  }
+void loop() { 
+  Menu+=getButton();
+    
+    
+    if (Menu==0)showClock();
+    if (Menu==64)showClima();
+    if (Menu==128)showTemp();
+    if (Menu==192)showVolt();
+  
+  
+  
+
 }
