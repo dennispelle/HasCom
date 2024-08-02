fa55df88fae519c0dc092bf498b4c64390aab1e5
Dennis Schwartze <dennis.schwartze@gmail.com>
2024-08-02 11:18:04 +0200

    Update Hascom.ino

--- Hascom.ino
+++ Hascom.ino
@@ -30,8 +30,15 @@ SimpleDHT11 dht11(2);
 LiquidCrystal lcd(3,4,5,6,7,8);
 byte jahr,monat,tag,stunde,mimute,sekunde,zeitzone=2;//Zeitzone für sommer/winterzeit usw
 byte uzeit=zeitzone+1;
+byte Menu;
+byte L,M,R;
 double uMin,uMax,uMinabs=1023,uMaxabs,uAvr,uNow,uRip,uRipabs;
 void setup() {
+
+  digitalWrite(A1,HIGH);
+  digitalWrite(A2,HIGH);
+  digitalWrite(A3,HIGH);
+  
   ss.begin(GPSBaud);
   lcd.begin(8, 2);
   sensors.begin();
@@ -138,7 +145,7 @@ void getVolt(){
   uMin=1023;
   uMax=0;
   for(x=0;x<200;x++){
-      uNow=5.0*analogRead(A1)/1023.0;
+      uNow=5.0*analogRead(A0)/1023.0;
       uAvr+=uNow;
       if (uNow>uMax)uMax=uNow;
       if (uNow<uMin)uMin=uNow;
@@ -163,16 +170,36 @@ void showTemp(){
   lcd.setCursor(0,1);
   lcd.print("T2=");lcd.print(sensors.getTempCByIndex(1));
 }
-void loop() {
+byte getButton(){
+  byte doit=0;
+  if (digitalRead(A1))L=0; else {L=1;doit=1;}
+  if (digitalRead(A2))M=0; else {M=1;doit=2;}
+  if (digitalRead(A3))R=0; else {R=1;doit=3;}
+  
+  if ((L+M+R)==1) return doit;
+  if (((L+M+R)==0)||((L+M+R)>1)) {doit=0;return doit;}
+    
+    
+  
+  }
+void goRound(){
+  
+  //Rotation der Menüs
+  }
+void loop() { 
+int doit;
+  if ((millis()%200)==0){
   sensors.requestTemperatures();// Hole Temperaturen der Tempsensoren
- 
-  showClima();
-  delay(2000);
-  showClock();
-  delay(2000);
-  getVolt();
-  showVolt();
-  delay(2000);
-  showTemp();
-  delay(2000);
+  getVolt();// Messe Boardspannung
+  if (Menu==0)showClock();
+  if (Menu==64)showClima();
+  if (Menu==128)showTemp();
+  if (Menu==192)showVolt();}
+  if (doit==0) doit=getButton();
+  
+  if ((millis()%100)==0){
+    if (doit==1) Menu+=64;
+    if (doit==3) Menu-=64;
+    doit=0;
+  }
 }
