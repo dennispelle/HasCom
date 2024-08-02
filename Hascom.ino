565ce73fda511d09b6aa5e1341bb3654afdd61e7
Dennis Schwartze <dennis.schwartze@gmail.com>
2024-08-02 11:17:16 +0200

    Update Hascom.ino

--- Hascom.ino
+++ Hascom.ino
@@ -5,9 +5,9 @@
 #include <TinyGPS++.h>
 // The TinyGPS++ object
 TinyGPSPlus gps;
-static const int RXPin = 12, TXPin = 11;
-static const uint32_t GPSBaud = 4800;
-
+static const int RXPin = 11, TXPin = 12;
+static const uint32_t GPSBaud = 9600;
+const int UTC_offset = 2;
 SoftwareSerial ss(RXPin, TXPin);
   /*initialisiere den dht11 Sensor (Temperatur 0-50°C;Luftfeuechte 20%-95%RH)
    * dht11.read(&temperature, &humidity, NULL)
@@ -23,13 +23,15 @@ SimpleDHT11 dht11(2);
  */
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(3,4,5,6,7,8);
+byte jahr,monat,tag,stunde,mimute,sekunde,zeitzone=2;//Zeitzone für sommer/winterzeit usw
+byte uzeit=zeitzone+1;
+double uMin,uMax,uMinabs=1023,uMaxabs,uAvr,uNow,uRip,uRipabs;
 void setup() {
   ss.begin(GPSBaud);
   lcd.begin(8, 2);
-  lcd.setCursor(0,0);
-  lcd.print("START");
+
 }
-void lcdClean(){ // LcdBildschirm Löschen
+void lcdclean(){ // LcdBildschirm Löschen
   lcd.setCursor(0,0);
   lcd.print("        ");
   lcd.setCursor(0,1);
@@ -38,9 +40,8 @@ void lcdClean(){ // LcdBildschirm Löschen
 void getTemp(){ // Hole die Tempereatur und die Luftfeuchtigkeit und speichere sie in Var: temperature und humidity
    err = dht11.read(&temperature, &humidity, NULL);
   }
-
 void showClima(){// Zeige Temperatur und Luftfeuchte im Fahrzeug an
-  lcdClean();
+  lcdclean();
   getTemp();
   lcd.setCursor(0,0);
  
@@ -50,25 +51,111 @@ void showClima(){// Zeige Temperatur und Luftfeuchte im Fahrzeug an
   lcd.print((int)humidity);
   lcd.print("% rel");
   }
-
-void showClock(){
-  lcdClean();
-  while (ss.available() > 0)
+void getClock(){
+    while (ss.available() > 0)
     if (gps.encode(ss.read()))
-    lcd.setCursor(0, 0);
-    if (gps.time.hour() < 10) lcd.print(F("0"));
-    lcd.print(gps.time.hour()+2);
-    lcd.print(F(":"));
-    if (gps.time.minute() < 10) lcd.print(F("0"));
-    lcd.print(gps.time.minute());
-    lcd.print(F(":"));
-    if (gps.time.second() < 10) lcd.print(F("0"));
-    lcd.print(gps.time.second());
+
+    //hole die aktuelle Zeit über GPS 
+
+    jahr=gps.date.year()-2000;
+    //jahr=19;//schaltjahr
+    monat=gps.date.month();
+    //monat=12;//testmonat
+    tag=gps.date.day();
+    //tag=31;
+    stunde=gps.time.hour();
+    //stunde=23;//überlaufstunde
+    mimute=gps.time.minute();
+    sekunde=gps.time.second();
+
+    //korrigiere die Zeit nach der Zeitzone
+    stunde=stunde+zeitzone;
+    stunde=stunde%24;
+   
+    if (stunde<uzeit)//überlaufzeit
+    {
+      tag++;     
+      if (tag==32){       tag=1;  monat++;} //wenn der monat 32tage hat
+      if (tag==31) //möglicher überlauftag
+      {
+        if (monat==4){    tag=1;  monat++;}
+        if (monat==6){    tag=1;  monat++;}
+        if (monat==9){    tag=1;  monat++;}
+        if (monat==11){   tag=1;  monat++;}
+      }
+      if (monat==2){
+        if (tag==30){     tag=1;  monat++;}
+        if (tag==29){if ((jahr%4)!=0){tag=1;monat++;}}
+        }   //wenn februar und kein schaltjahr
+      //if ((monat==2)&&((jahr%4)==0)&&(tag==30)){tag=1; monat++;}  //wenn februar und schaltjahr
+
+      if (monat==13){monat=1;jahr++;}
+          
+    }
+
+
+          
+}
+void showClock(){
+  getClock();
+    lcdclean();  
+// Kein Datum nur die Uhrzeit   
+    lcd.setCursor(0,0);    
+    lcd.print("Uhrzeit:");
+/*    
+    if (tag<10) lcd.print("0");
+    lcd.print(tag); lcd.print(".");
+    
+    if (monat<10) lcd.print("0");
+    lcd.print(monat);lcd.print(".");
+        
+    lcd.print(jahr);
+    */
+    lcd.setCursor(1, 1); 
+//  lcd.setCursor(0,1); //falls sekunden angezeigt werden sollen
+    if (stunde<10) lcd.print("0");
+    lcd.print(stunde); lcd.print(":");
+    
+    if (mimute<10) lcd.print("0");
+    lcd.print(mimute);
+    
+/*
+    lcd.print(":");
+    if (sekunde<10) lcd.print("0");
+    lcd.print(sekunde);
+*/
   
+  }  
+void getVolt(){
+  int x;
+  uAvr=0;
+  uMin=1023;
+  uMax=0;
+  for(x=0;x<200;x++){
+      uNow=5.0*analogRead(A1)/1023.0;
+      uAvr+=uNow;
+      if (uNow>uMax)uMax=uNow;
+      if (uNow<uMin)uMin=uNow;
+    }
+   uAvr/=200;
+   uRip=uMax-uMin;
+   if (uRipabs<uRip)uRipabs=uRip;
+   if (uMaxabs<uMax)uMaxabs=uMax;
+   if (uMinabs>uMin)uMinabs=uMin;
+  }
+void showVolt(){
+  lcdclean();
+  lcd.setCursor(0,0);
+  lcd.print("Um=");lcd.print(uAvr);lcd.print("V");
+  lcd.setCursor(0,1);
+  lcd.print("Ud=");lcd.print(uRip);lcd.print("V");
   }
 void loop() {
+  showClima();
+  delay(1000);
   showClock();
-  delay(5000);
-  
-
+  delay(1000);
+  getVolt();
+  showVolt();
+  delay(1000);
 }
